deb57997d57223c36a9eb46d2f32bb023c23f156
Azalee Bostroem <bostroem@stsci.edu>
2012-12-07 16:21:24 +0000

    initial commit for combine_dithered_images and interactive_extraction

---
+++ combine_dithered_images.py
@@ -0,0 +1,172 @@
+import pyfits
+import numpy as np
+import os
+import glob
+import c_correlate 
+
+#!!!!!!!!!!!!!!!!!!!!!!
+#CORRECTNESS OF DQ AND ERR ARRAYS HAVE NOT BEEN CONFIRMED ALTHOUGH THEY SHOULD FOLLOW THE SAME
+#RULES AS THE IMAGE EXTENSION
+#!!!!!!!!!!!!!!!!!!!!!!
+
+
+def make_declination_dict(flist):
+    '''
+    Create a dictionary with declination as keys and filenames as lists to each key
+    This is to match 2 dithered images together
+
+    Create a dictionary with declination as keys and filenames as lists to each key
+    This is to match 2 dithered images together
+
+    Called from: Main
+    Calls to: Nothing
+
+    Input:
+        flist: list of files to make dictionary out of
+    Outputs:
+        Nothing
+    Returns:
+        dec_dict: a dictionary with declination as keys and filenames as lists to each key
+    '''
+    dec_dict = {} 
+    for ifile in flist:
+        targ_dec = pyfits.getval(ifile, 'dec_targ', 0)
+        if dec_dict.has_key(targ_dec):
+            dec_dict[targ_dec].append(ifile)
+        else:
+            dec_dict[targ_dec] = [ifile]
+    return dec_dict
+
+def combine_dithered_images(dec_dict, targ_dec):
+    '''
+    The 12465 data is dithered along the slit direction. This program finds the shifts
+    of the dithered images by cross-correlating a middle column and combines them.
+
+    Called from: 
+        extract_spectrum
+    Calls to: 
+        c_correlate (external module)
+        make_combined_fits
+
+    Input:
+        dec_dict: dictionary with declination as keys and a list of filenames of data taken at 
+            the that declination as values
+        idir: directory where the data is stored
+        cenwave (optional): central wavelength - this is for CCD data which is taken at multiple cenwaves
+    Outputs:
+        None
+    Returns:
+        new_img: combined image array
+    '''
+
+    #Read in 2 images
+    ofile1 = pyfits.open(dec_dict[targ_dec][0])
+    ofile2 = pyfits.open(dec_dict[targ_dec][1])
+    hdr0 = ofile1[0].header 
+    hdr1 = ofile1[1].header
+    hdr2 = ofile1[2].header
+    hdr3 = ofile1[3].header
+    img1 = ofile1[1].data
+    err1 = ofile1[2].data
+    dq1 = ofile1[3].data
+    img2 = ofile2[1].data
+    err2 = ofile2[2].data
+    dq2 = ofile2[3].data
+
+    cenwave = hdr0['cenwave']
+    exptime2 = pyfits.getval(dec_dict[targ_dec][1], 'texptime', 0)
+    hdr0['texptime'] = hdr0['texptime'] + exptime2  #exptime is only in the Primary HDU
+
+    #Get middle columns of each image
+    col1 = np.sum(img1[:, 350:650], axis = 1)
+    col2 = np.sum(img2[:, 350:650], axis = 1)
+    #Calculate offset
+    lag, corr_mat = c_correlate.c_corr(col1, col2)
+    max_indx = np.argmax(corr_mat)
+    print 'Found offset of %i' %(lag[max_indx])
+    if lag[max_indx] == 0:
+        lag = np.append(lag[:max_indx], lag[max_indx+1:])
+        corr_mat = np.append(corr_mat[:max_indx], corr_mat[max_indx+1:])
+        max_indx = np.argmax(corr_mat)
+        print 'Found new offset of %i' %(lag[max_indx])
+    if lag[max_indx] > 0:
+        start1 = 0
+        end1 = np.shape(img1)[0]
+        start2 = lag[max_indx]
+        end2 = np.shape(img1)[0]+lag[max_indx]
+        new_img, new_err, new_dq = create_new_combined_arrays(img1, img2, err1, err2, dq1, dq2, start1, end1, start2, end2)
+
+    else:
+        start1 = 0
+        end1 = np.shape(img1)[0]
+        start2 = np.abs(lag[max_indx])
+        end2 = np.shape(img1)[0]+np.abs(lag[max_indx])
+        new_img, new_err, new_dq = create_new_combined_arrays(img2, img1, err2, err1, dq2, dq1, start1, end1, start2, end2)
+
+    #Write fits file
+    make_combined_fits(hdr0, hdr1, hdr2, hdr3, new_img, new_err, new_dq, cenwave)
+ 
+    return new_img
+
+def create_new_combined_arrays(top_img, bottom_img, top_err, bottom_err, top_dq, bottom_dq, start1, end1, start2, end2):
+        new_img = np.zeros((end2, end1))
+        new_img[start1:end1, :] = new_img[start1:end1, :] + bottom_img
+        new_img[start2:end2, :] = new_img[start2:end2, :] + top_img
+
+        new_err1 = np.zeros((end2, end1))
+        new_err2 = np.zeros((end2, end1))
+        new_err1[start1:end1, :] = new_err1[start1:end1, :] + bottom_err
+        new_err2[start2:end2, :] = new_err2[start2:end2, :] + top_err
+        new_err = np.sqrt(new_err1**2 + new_err2**2)
+
+        new_dq1 = np.zeros((end2, end1))
+        new_dq2 = np.zeros((end2, end1))
+        new_dq1[start1:end1, :] = new_dq1[start1:end1, :] + bottom_dq
+        new_dq2[start2:end2, :] = new_dq2[start2:end2, :] + top_dq
+        new_dq1 = np.int_(new_dq1)
+        new_dq2 = np.int_(new_dq2)
+        new_dq = np.bitwise_or(new_dq1, new_dq2)
+        return new_img, new_err, new_dq
+
+
+def make_combined_fits(hdr0, hdr1, hdr2, hdr3, new_img, new_err, new_dq, cenwave):
+    '''
+    write combined image to file
+    Called from:
+        combine_dithered_images
+    Calls to:
+        Nothing
+    Inputs:
+        hdr: fits header object
+        img: image array
+        cenwave: central wavelength for filename
+    Outputs:
+        TARGNAME_(CENWAVE)_combined_img.fits files of the combined dithered images
+    Returns:
+        Nothing
+
+    '''
+    hdu0 = pyfits.PrimaryHDU()
+    hdu0.header = hdr0
+    print '#############################%s######################' %(hdr0['targname'])
+    hdu1 = pyfits.ImageHDU(data=new_img, name='SCI', header = hdr1)
+    hdu2 = pyfits.ImageHDU(data=new_err, name='ERR', header = hdr2)
+    hdu3 = pyfits.ImageHDU(data = np.int16(new_dq), name = 'DQ', header = hdr3)
+    hdulist = pyfits.HDUList([hdu0, hdu1, hdu2, hdu3])
+    if cenwave:
+        hdulist.writeto('%s_%i_combined_img.fits' %(hdr0['targname'], hdr0['cenwave']), clobber = True)
+    else:
+        hdulist.writeto('%s_combined_img.fits' %(hdr0['targname']), clobber = True)
+
+
+
+if __name__ == "__main__":
+
+    #for the FUV data
+    idir = '/Users/bostroem/science/12465_otfr20120425/mama/'
+    os.chdir(idir)
+    flist = glob.glob('obrc04???_flt.fits')+glob.glob('obrc05???_flt.fits')
+    dec_dict = make_declination_dict(flist)
+    for targ_dec in dec_dict.keys():
+        combine_dithered_images(dec_dict, targ_dec)
+
---
+++ interactive_extraction.py
@@ -0,0 +1,192 @@
+import pyfits
+import numpy as np
+import sys
+import math
+from matplotlib import pyplot
+import pdb
+import os
+import shutil
+
+import pyraf
+from pyraf import iraf
+from iraf import stsdas,hst_calib,stis,x1d
+
+
+def collapse_spectrum(img):
+    collapsed_img = np.sum(img, axis = 1)/1024
+    return collapsed_img
+
+def get_background_locations(fig1, ax1, pix_num, collapsed_img, filename):
+    background_collapsed = np.empty((0,))
+    background_pix = np.empty((0,))
+    finished_flag = 'n'
+    b_filename = filename.split('/')[-1][0:8]
+    if os.path.exists(os.path.join(os.getcwd(), '%s_background_regions.txt' %(b_filename))):
+        keep_file_flag = raw_input('Background file already exists: Overwrite file (w) or Append to file (a)')
+    else:
+        keep_file_flag = 'w'
+    ofile = open('%s_background_regions.txt' %(b_filename), keep_file_flag)
+    while finished_flag != 'y':
+        print 'Select background points for cross-dispersion background subtraction'
+        temp_region = fig1.ginput(n = 2, timeout = -1)
+        back_reg = [temp_region[0][0], temp_region[1][0]]
+        l1 = pyplot.plot(back_reg, np.interp(np.array(back_reg), pix_num, collapsed_img), 'r|-', markersize = 5)
+        pyplot.draw()
+        keep_flag = raw_input('Keep points? (y), n ')
+        if keep_flag != 'n':
+            ofile.write('%i, %i \n' %(int(math.floor(temp_region[0][0])),int(math.ceil(temp_region[1][0]))))
+            background_collapsed = np.append(background_collapsed, collapsed_img[int(math.floor(temp_region[0][0])):int(math.ceil(temp_region[1][0]))])
+            background_pix = np.append(background_pix, pix_num[int(math.floor(temp_region[0][0])):int(math.ceil(temp_region[1][0]))])
+        else:
+            l1[0].set_visible(False)
+            pyplot.draw()
+        finished_flag = raw_input('Finished entering points? (n), y ')
+    ofile.close()
+    sort_indx = np.argsort(background_pix)
+    background_pix = background_pix[sort_indx]
+    background_collapsed = background_collapsed[sort_indx]
+
+    return fig1, ax1, background_pix, background_collapsed
+
+def fit_background(ax1, background_pix, background_collapsed, pix_num):
+    deg = 1
+    change_deg_flag = 'y'
+    leg_text = ['%i' %(deg)]
+    leg_lines = []
+    while change_deg_flag != 'n':
+        coeff = np.polyfit(background_pix, background_collapsed, deg)
+        fit = np.polyval(coeff, pix_num)
+        l1 = ax1.plot(pix_num, fit, lw = 2)
+        leg_lines.append(l1[0])
+        pyplot.legend(leg_lines, leg_text, loc = 'best')
+        pyplot.draw()
+        change_deg_flag = raw_input('Replot with a different degree polynomial? (y), n ')
+        if change_deg_flag != 'n':
+            
+            l1[0].set_linestyle('--')
+            #l1[0].set_visible(False)
+            pyplot.draw()
+
+            try:
+                deg = int(raw_input('Enter degree of polynomial you wish to fit: '))
+            except ValueError:
+                print 'The value you entered cannot be converted to an integer, try again'
+                deg = int(raw_input('Enter degree of polynomial you wish to fit: '))
+
+            leg_text.append('%i' %(deg))
+    return ax1, fit
+
+def subtract_2D_cross_disp_background(img, fit):
+    two_D_background = np.rot90(np.rot90(np.rot90(np.tile(fit, (1024, 1)))))
+    return img - two_D_background
+
+
+def write_temp_subtracted_file(subtracted_img, filename):
+    shutil.copyfile(filename, filename.replace('.fits', 'sub.fits'))
+    ofile = pyfits.open(filename.replace('.fits', 'sub.fits'), mode = 'update')
+    ofile[1].data = subtracted_img
+    ofile.flush()
+    ofile.close()
+
+def select_extraction_location(fig1, c):
+    raw_input('Zoom in on spectrum, hit Enter when finished')
+    print 'Select spectrum location'
+    keep_flag = 'n'
+    while keep_flag == 'n':
+        coords = fig1.ginput(n = 1, timeout = -1)
+        ln = pyplot.plot([coords[0][0]], [coords[0][1]], '%s+' %(c))
+        keep_flag = raw_input('Keep selection? (y), n ')
+        if keep_flag == 'n':
+            ln[0].set_visible(False)
+            pyplot.draw()
+
+    return coords[0][0]
+    
+
+def select_extraction_box_size(fig1, extrlocy, c):
+    print 'Select extraction box'
+    keep_flag = 'n'
+    while keep_flag == 'n':
+        coords = fig1.ginput(n = 2, timeout = -1)
+        half_box = min(abs(coords[0][0] - extrlocy), abs(coords[1][0] - extrlocy))
+        lb = pyplot.plot([extrlocy - half_box, extrlocy + half_box],[coords[0][1], coords[0][1]] , '%s|-' %(c), markersize = 10)
+        pyplot.draw()
+        keep_flag = raw_input('Keep points? (y), n ')
+        if keep_flag == 'n':
+            lb[0].set_visible(False)
+            pyplot.draw()
+    return half_box*2
+
+def extract_spectrum(extrlocy, extract_box_size, background_loc1, background_size1, background_loc2, background_size2, filename, c):
+    cenwave = pyfits.getval(filename, 'cenwave', 0)
+    if os.path.exists(os.path.join(os.getcwd(), filename.replace('.fits', 'loc%i_%i.fits' %(int(extrlocy), cenwave)))):
+        os.remove(os.path.join(os.getcwd(), filename.replace('.fits', 'loc%i_%i.fits' %(int(extrlocy), cenwave))))
+        
+    iraf.stsdas.hst_calib.stis.x1d(filename.replace('.fits', 'sub.fits'), output = filename.replace('.fits', 'loc%i_%i.fits' %(int(extrlocy), cenwave)), \
+                                       a2center = extrlocy, extrsize = extract_box_size, maxsrch = 0, bk1offst = background_loc1 - extrlocy, \
+                                       bk2offst = background_loc2 - extrlocy, bk1size = background_size1, bk2size = background_size2)
+ 
+
+if __name__ == "__main__":
+    colors = ['r', 'g', 'c', 'k', 'm']
+
+    pyplot.ion()
+    os.environ['oref'] = '/Users/bostroem/science/oref/'
+    filename = sys.argv[1]
+    try:
+        ext = int(sys.argv[2])
+    except:
+        ext = 1
+    img = pyfits.getdata(filename, ext)
+    collapsed_img = collapse_spectrum(img)
+    fig1 = pyplot.figure(1)
+    ax1 = fig1.add_subplot(1, 1, 1)
+    pix_num = range(len(collapsed_img))
+    ax1.plot(pix_num, collapsed_img)
+    ax1.set_xlabel('Pixel Number')
+    ax1.set_ylabel('Total Counts Summed in the Dispersion Direction')
+    disp_2d = raw_input('Would you like to display the 2D image? y, (n)')
+    if disp_2d == 'y':
+        fig2 = pyplot.figure(2)
+        ax2 = fig2.add_subplot(1,1,1)
+        ax2.imshow(np.rot90(np.log10(img)), interpolation = 'nearest', origin = 'lower')
+    raw_input('Zoom in on desired spectrum location. Press Enter when finished')
+    fig1, ax1, background_pix, background_collapsed = get_background_locations(fig1, ax1, pix_num, collapsed_img, filename)
+    
+    xlims = ax1.get_xlim()
+    ylims = ax1.get_ylim()
+    ax1, fit = fit_background(ax1, background_pix, background_collapsed, pix_num)
+    subtracted_img = subtract_2D_cross_disp_background(img, fit)
+    write_temp_subtracted_file(subtracted_img, filename)
+    sub_collapsed_img = collapse_spectrum(subtracted_img)
+    ax1.cla()
+    ax1.plot(pix_num, sub_collapsed_img)
+    another_spectrum_flag = 'y'
+    i = 0
+    while another_spectrum_flag != 'n':
+        c = colors[i%len(colors)]
+        ax1.set_xlim(xlims[0], xlims[1])
+        ax1.set_ylim(ylims[0], ylims[1])
+        pyplot.draw()
+        print 'Identify spectrum location'
+        extrlocy = select_extraction_location(fig1, c)
+        print 'Identify extraction box'
+        extract_box_size = select_extraction_box_size(fig1, extrlocy, c)
+        print 'Identify left background center'
+        background_loc1 = select_extraction_location(fig1, c)
+        print 'Idnetify left background box'
+        background_size1 = select_extraction_box_size(fig1, background_loc1, c)
+        print 'Identify right background center'
+        background_loc2 = select_extraction_location(fig1, c)
+        print 'Identify right background box'
+        background_size2 = select_extraction_box_size(fig1, background_loc2, c)
+        extract_spectrum(extrlocy, extract_box_size, background_loc1, background_size1, background_loc2, background_size2, filename, c)
+        another_spectrum_flag = raw_input('Extract another spectrum?')
+        i = i + 1
+    os.remove(filename.replace('.fits', 'sub.fits'))
+
+
+
+
+    raw_input('Enter')
+
