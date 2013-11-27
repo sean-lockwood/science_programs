5a57a0548037f01b3c2822cf570cef137dd1c423
Azalee Bostroem <bostroem@stsci.edu>
2013-11-27 11:19:21 -0500

    Edited to fix bugs and run pipeline end to end Moved imports to the top of the program so I could run individual functions interactively Code now writes how long it took to run each step Added more options to the call to separate_obs to move flc files and copy not mv files to directory to create reference files Added function make_reffiles instead of calling the make_reffiles function in REFSTIS_wrapper. This was enabled by a refactor of the make_reffiles in REFSTIS_wrapper. It gives more flexibility in the options called to make each reference file type. Added functionality to assign the appropriate reference file to an observation Calstis and basic2D now write the trailer to log files The code removed calibration products before running basic2d Code now runs calibration pipeline with CTE correction in the middle

--- cte_correction_pipeline.py
+++ cte_correction_pipeline.py
@@ -2,10 +2,36 @@ import os
 import shutil
 import glob
 import sys
-import pyfits
+try:
+    import pyfits
+except:
+    from astropy.io import fits as pyfits
 import numpy as np
 import pdb
 import time
+import datetime
+from astropy.time import Time
+import stistools
+
+#Set paths
+#Eventually this can be done with argparse
+#set cur_dir, refstis_dir, 
+#What do I use programs for?
+
+cur_dir = '/user/bostroem/science/cte/2012_04'
+refstis_dir = '/user/bostroem/science/programs/refstis/'
+ctecorr_dir = '/user/bostroem/science/programs/ctecorr/stis_mac/lib/stistools'
+os.environ['myref'] = os.path.join(cur_dir, 'reffiles')+'/'
+herringbone_dir = os.path.join(cur_dir, 'herringbone_correction')
+sys.path.append('/user/bostroem/science/programs')
+sys.path.append(herringbone_dir)
+sys.path.append(refstis_dir)
+sys.path.append(ctecorr_dir)
+pctetab = 'stis_apr_2012_pcte.fits'
+import REFSTIS_wrapper
+import batch_run_autofilet
+from REFSTIS_functions import divide_anneal_month
+import StisPixCteCorr
 
 
 
@@ -27,11 +53,13 @@ def apply_herringbone_correction(input_dir, input_flist, output_dir = None):
         shutil.copy(os.path.join(input_dir, ifile), 'herringbone_correction/ORIG/%s' %(ifile))
     #Run autofilet
     os.chdir(herringbone_dir)
+    print 'Starting Herringbone Correction'
     batch_run_autofilet.autofilet()
-    batch_run_autofilet.rebuild_files(interactive = True)
+    batch_run_autofilet.rebuild_files(interactive = False)
     #Move original raw files to the orig folder and corrected files to input_dir
     os.chdir(os.path.join(cur_dir, input_dir))
-    os.mkdir('orig')
+    if not os.path.exists('orig'):
+        os.mkdir('orig')
     for ifile in input_flist:
         shutil.copy(ifile, os.path.join(cur_dir, input_dir, 'orig/%s' %(ifile))) #copy uncorrected to orig
         shutil.copy(os.path.join(herringbone_dir, 'CLEAN/%s' %(ifile)), os.path.join(cur_dir, output_dir, ifile)) #copy corrected to input_dir
@@ -59,28 +87,54 @@ def create_reference_file(input_dir, input_flist):
     for ifile in input_flist:
         date.append(pyfits.getval(os.path.join(input_dir, ifile), 'texpstrt', 0))
         date.append(pyfits.getval(os.path.join(input_dir, ifile),'texpend', 0))
+    filetype = pyfits.getval(os.path.join(input_dir, ifile), 'targname', 0)
     date = np.array(date)
     month_begin = np.min(date)
     month_end = np.max(date)
-    REFSTIS_wrapper.separate_obs(input_dir, month_begin, month_end)
-    start = time.time()
-    REFSTIS_wrapper.make_ref_files(input_dir)
-    end = time.time()
-    print 'RUNTIME = %f minutes' %((end - start)/60.0) 
+    if filetype == 'DARK':
+        REFSTIS_wrapper.separate_obs(input_dir, month_begin, month_end, input_filetype = 'flc', move_files = False)
+    else:
+        REFSTIS_wrapper.separate_obs(input_dir, month_begin, month_end, move_files = False)
+    make_reffiles(input_dir, filetype)
+
     #Copy reference file from each week folder to the reffiles folder
     os.path.walk(input_dir, copy_reference_files_to_reffiles, '')   
+
+def make_reffiles(root_folder, filetype):
+    """ Make all refrence files for a given folder
+
+    This functions is very specific to the REFSTIS pipeline, and requires files
+    and folders to have certain naming conventions.  
+
+    """
+    print '#-----------------------------#'
+    print '#  Making all ref files for   #'
+    print  root_folder
+    print '#-----------------------------#'
+
+    if not os.path.exists( root_folder ): raise IOError( 'Root folder: %s does not exist' %(root_folder))
+    gain_folders, week_folders = REFSTIS_wrapper.pull_out_subfolders( root_folder )
+    if filetype.upper() == 'BIAS':
+        REFSTIS_wrapper.refbias(week_folders)
+        #Basebias is not currently supported   
+        #REFSTIS_wrapper.basebias(root_folder, gain_folders)
+    elif filetype.upper() == 'DARK':
+        REFSTIS_wrapper.basedark(root_folder, input_filetype = 'flc', set_biasfile = False)
+        REFSTIS_wrapper.weekdark(week_folders, input_filetype = 'flc', set_biasfile = False)
+    else:
+        sys.exit('ERROR: File type not recognized')
 
 def copy_reference_files_to_reffiles(junk, dirname, junk2):
     '''
     This function copies super bias and super dark reference files to the reffiles folder
     '''
-    flist = glob.glob(os.path.join(dirname, 'ref*.fits')) + glob.glob(os.path.join(dirname, 'week*.fits'))
+    flist = glob.glob(os.path.join(dirname, '*bia.fits')) + glob.glob(os.path.join(dirname, '*drk.fits'))
     if len(flist) > 0:
         for ifile in flist:
             shutil.copy(ifile, os.path.join(cur_dir, 'reffiles', ifile.split('/')[-1]))
             print 'Copying %s to reffiles folder' %(ifile)
 
-def calibrate_data_with_cte_correction(input_flist, header_tup = None):
+def calibrate_data_with_cte_correction(input_dir, input_flist):
     '''
     This function will:
     1. copy relevant reference files
@@ -89,94 +143,210 @@ def calibrate_data_with_cte_correction(input_flist, header_tup = None):
     4. Run the CTE correction code
     5. Finish running calstis on the CTE corrected files with CTECORR turned off
     '''
-    copy_relevant_reference_files(reffile_list, output_dir)
-    update_header_keywords(input_flist, header_tup)
-    run_calstis_part1(input_flist)
-    run_cte_correction_code(input_flist)
-    run_calstis_part2(input_flist)
+    #copy_relevant_reference_files(reffile_list, output_dir)
+    update_reffile_keywords(input_dir, input_flist, 'bia')
+    try:
+        update_reffile_keyword(input_dir, input_flist, 'drk')
+    except:
+        pass
+    add_pctetab_to_headers(input_dir, input_flist)
+    run_calstis_part1(input_dir, input_flist)
+    run_cte_correction_code(input_dir, input_flist)
+    run_calstis_part2(input_dir, input_flist)
 
-def determine_correct_reference_files(input_dir, input_list):
-    for ifile in flist:
+
+def update_reffile_keywords(input_dir, input_list, filetype):
+    '''
+    This function takes the files in input_dir/input_flist and updates
+    the reference files associated with filetype 
+    Inputs:
+        input_dir: directory of files whose headers you want to update
+        input_list: names of files whose headers you want to update
+        filetype: either drk or bia - the reference file whose name you want to update
+    Outputs:
+        modifies files in input_flist
+    '''
+    #Get dates for reference file application as a function of observation mode
+    mode_dict = determine_correct_reference_files(input_dir, input_list, filetype)
+    #Determine which keyword to update
+    assert (filetype is 'bia') or (filetype is 'drk'), 'ERROR: Unknown filetype %s, please use bia or drk' %(filetype)
+    filetype_dict = {'bia':'BIASFILE', 'drk':'DARKFILE'}
+    keyword = filetype_dict[filetype]
+    #Update headers
+    for ifile in input_list:
+        hdr0 = pyfits.getheader(os.path.join(input_dir, ifile), 0)
+        gain = hdr0['ccdgain']
+        binaxis1 = hdr0['binaxis1']
+        binaxis2 = hdr0['binaxis2']
+        expstart = hdr0['texpstrt']
+        useafter_dates = mode_dict[gain][binaxis1][binaxis2].keys()
+        useafter_dates.sort()
+        useafter_dates = np.array(useafter_dates)
+        date_diff = expstart - useafter_dates 
+        date_indx = np.where((date_diff > 0))
+        reffile = mode_dict[gain][binaxis1][binaxis2][useafter_dates[date_indx[0][-1]]]
+        pyfits.setval(os.path.join(input_dir, ifile), keyword, value = reffile, ext = 0)
+
+def determine_correct_reference_files(input_dir, input_list, filetype):
+    '''
+    This function creates a nested dictionary which assigns a reference file name (from 
+    the reffiles folder) to each gain, binaxis1, binaxis2, useafter combination
+    Inputs:
+        input_dir: directory of files whose headers you want to update
+        input_list: names of files whose headers you want to update
+        filetype: either drk or bia - the reference file whose name you want to update
+    Output:
+        mode_dict: a nested dictionary which hich assigns a reference file name (from 
+    the reffiles folder) to each gain, binaxis1, binaxis2, useafter combination
+
+    This code assumes that you have set the environment variable myref to point to the 
+    reffiles folder
+    '''
+    dates = []
+    for ifile in input_list:
         filename = os.path.join(input_dir, ifile)
-        date = pyfits.getval(ifile, 'expstart', 0)
+        dates.append(pyfits.getval(filename, 'texpstrt', 0))
+        dates.append(pyfits.getval(filename, 'texpend', 0))
+    data_start = min(dates)
+    data_end = max(dates)
+    anneal_weeks_4 = divide_anneal_month(data_start, data_end, '/grp/hst/stis/calibration/anneals/', 4)
+    anneal_weeks_2 = divide_anneal_month(data_start, data_end, '/grp/hst/stis/calibration/anneals/', 2)
+    #nested dictionary: gain, binaxis1, binaxis2, week
+                #gain
+    mode_dict = {1:
+                    #binaxis1
+                    {1:
+                        #binaxis2
+                        #1x1 gain = 1
+                        {1:{round(anneal_weeks_4[0][0], 4):'', round(anneal_weeks_4[1][0], 4):'', round(anneal_weeks_4[2][0], 4):'', round(anneal_weeks_4[3][0], 4):''},   
+                        # 1x2 gain = 1
+                        2:{round(anneal_weeks_2[0][0], 4):'', round(anneal_weeks_2[1][0], 4):''}},    
+                    2: 
+                        #2x1 gain = 1
+                        {1:{round(anneal_weeks_2[0][0], 4):'', round(anneal_weeks_2[1][0], 4):''},  
+                        #2x2 gain = 1
+                        2:{round(anneal_weeks_2[0][0], 4):'', round(anneal_weeks_2[1][0], 4):''}}}, 
+                4:  
+                    #binaxis1
+                    {1:
+                        #binaxis2
+                        #1x1 gain = 4
+                        {1:{round(anneal_weeks_2[0][0], 4):'', round(anneal_weeks_2[1][0], 4):''}}}}   
+    reffile_list = glob.glob(os.path.join(os.environ['myref'], '*_%s*.fits' %(filetype)))
+    for reffile in reffile_list:
+        hdr0 = pyfits.getheader(reffile, 0)
+        gain = hdr0['ccdgain']
+        binaxis1 = hdr0['binaxis1']
+        binaxis2 = hdr0['binaxis2']
+        useafter = convert_useafter_to_mjd(hdr0['useafter'])
+        mode_dict[gain][binaxis1][binaxis2][round(useafter, 4)] = 'myref$%s' %(reffile.split('/')[-1])
+                       
+    return mode_dict
+
+def convert_useafter_to_mjd(useafter_date):
+    useafter_reformat = datetime.datetime.strptime(useafter_date, '%b %d %Y %X').strftime('%Y-%m-%d %X')
+    useafter_mjd = Time(useafter_reformat, format = 'iso', scale = 'utc').mjd
+    return useafter_mjd
         
 
-def copy_relevant_reference_files(reffile_list, output_dir):
-    '''
-    This file copies all reference files in reffile_list from reffiles to output_dir
-    '''
-    
-
-def update_header_keywords(input_flist, header_tup):
-    '''
-    This function takes every header (keyword, value) tuple and updates every file in 
-    input_flist
-    '''
-    pass
-
-def run_calstis_part1(input_flist):
+def remove_calibration_products(filename):
+    try:
+        os.remove(filename.replace('raw', 'flt'))
+    except:
+        pass
+    try:
+        os.remove(filename.replace('raw', 'crj'))
+    except:
+        pass
+    try:
+        os.remove(filename.replace('raw', 'flc'))
+    except:
+        pass
+def run_calstis_part1(input_dir, input_flist):
     '''
     This function runs BIASCORR, BLEVCORR and DQICORR on the files in input_flist
     '''
-    pass
+    log = open('calstis_log.txt', 'a')
+    log.write('calstis_part1 - basic2d')
+    log.close()
+    for ifile in input_flist:
+        filename = os.path.join(input_dir, ifile)
+        remove_calibration_products(filename)
 
-def run_cte_correction_code(input_flist):
+        stistools.basic2d.basic2d(filename, dqicorr = 'perform', blevcorr = 'perform', biascorr = 'perform',
+                atodcorr = 'omit', doppcorr = 'omit', lorscorr = 'omit', glincorr = 'omit', lflgcorr = 'omit', 
+                darkcorr = 'omit', flatcorr = 'omit', shadcorr = 'omit', photcorr = 'omit', trailer = 'calstis_log.txt')
+
+def run_cte_correction_code(input_dir, input_flist):
     '''
     This function will run the CTE correction code StisPixCteCorr.py on each file in 
     input_flist
     '''
-    pass
+    
+    for ifile in input_flist:
+        filename = os.path.join(input_dir, ifile)
+        StisPixCteCorr.CteCorr(filename.replace('raw', 'flt'),outFits = filename.replace('raw', 'flc') )
+        pyfits.setval(filename.replace('raw', 'flc'), 'CTECORR', ext = 0, value = 'COMPLETE')
 
-def run_calstis_part2(input_flist):
+def run_calstis_part2(input_dir, input_flist):
     '''
     This function finishes running calstis on the CTE corrected data. Note: CTECORR should be
     set to OMIT
     '''
-    pass
+    #biacorr, blevcorr, and dqicorr are set to complete after part1 is called. I can call calstis
+    log = open('calstis_log.txt', 'a')
+    log.write('calstis_part2 - calstis')
+    log.close()
+    for ifile in input_flist:
+        filename = os.path.join(input_dir, ifile)
+        stistools.calstis.calstis(filename.replace('raw', 'flc'), trailer = 'calstis_log.txt')
+
+def add_pctetab_to_headers(input_dir, input_flist):
+    for ifile in input_flist:
+        pyfits.setval(os.path.join(input_dir, ifile), 'PCTETAB', ext = 0, value = 'myref$%s' %(pctetab))
 
 if __name__ == "__main__":
 
-    #Set paths
-    #Eventually this can be done with argparse
-    #set cur_dir, refstis_dir, 
-    #What do I use programs for?
 
-    cur_dir = '/user/bostroem/science/cte/2012_04'
-    refstis_dir = '/user/bostroem/science/programs/refstis/'
-    herringbone_dir = os.path.join(cur_dir, 'herringbone_correction')
-    sys.path.append('/user/bostroem/science/programs')
-    sys.path.append(herringbone_dir)
-    sys.path.append(refstis_dir)
-    import REFSTIS_wrapper
-    import batch_run_autofilet
     os.chdir(cur_dir)
-
+    start = time.time()
     #Herringbone correct Bias files
     os.chdir('bias')
     flist_bias = glob.glob('*raw.fits')
     os.chdir(cur_dir)
-    ##apply_herringbone_correction('bias', flist_bias)
+    apply_herringbone_correction('bias', flist_bias)
 
     
     #Create Superbias
-    #create_reference_file('bias', flist_bias)
+    create_reference_file('bias', flist_bias)
     
+    end = time.time()
+    print 'RUNTIME BIAS = %f minutes' %((end - start)/60.0) 
+
+    start = time.time()
     #Herringbone correct Dark files
     os.chdir('dark')
     flist_dark = glob.glob('*raw.fits')
     os.chdir(cur_dir)
     apply_herringbone_correction('dark', flist_dark)
-    '''pwd
-    #CTE correct and calibrate Dark files with new superbias
-    calibrate_data_with_cte_correction(flist_dark, header_tup = [('pcttab', 'stis_apr_2012_pcte.fits'), ('biasfile', 'superbias.fits')])
-    #Create Superdark
-    create_reference_file('dark')
     
+    #CTE correct and calibrate Dark files with new superbias
+    calibrate_data_with_cte_correction(os.path.join(cur_dir, 'dark'), flist_dark)
+    
+    
+    #Create Superdark
+    create_reference_file('dark', flist_dark)
+    end = time.time()
+    print 'RUNTIME DARK = %f minutes' %((end - start)/60.0)     
+
+    start = time.time()
     #Herringbone correct science data
     os.chdir('sci_data')
     flist_sci = glob.glob('*raw.fits')
     os.chdir(cur_dir)
     apply_herringbone_correction('sci_data', flist_sci)
     #CTE correcte and calibrate science data with new superbias and superdark
-    calibrate_data_with_cte_correction(flist_sci, header_tup = [('pcttab', 'stis_apr_2012_pcte.fits'), ('biasfile', 'superbias.fits'), ('darkfile', 'superdark.fits')])
-    '''
+    calibrate_data_with_cte_correction(os.path.join(cur_dir, 'sci_data'), flist_sci)
+    end = time.time()
+    print 'RUNTIME Science= %f minutes' %((end - start)/60.0) 
+    
